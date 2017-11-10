fd001f5d9f39223225525fc3e15a290158c790e4
sanjeet <you@example.com>
2017-11-10 14:29:30 +0530

    gmail mailer updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
- default from: 'helpermail41@gmail.com'
+ default from: 'askkaro.help@gmail.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
