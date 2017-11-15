146ae4dd11523975605106d3849c641fdd8c5a61
sanjeet <you@example.com>
2017-11-16 01:26:28 +0530

    follow and tag card updated

--- db/migrate/20170723084333_create_questions.rb
+++ db/migrate/20170723084333_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :title,null: false
-      t.text :content,null: false
+      t.string :title, unique: true,null: false
+      t.text :content, unique: true,null: false
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
